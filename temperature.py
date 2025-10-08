0f84ddccf2f7a902c4eac930b924b58b410b4d2a
Madina Abdusamatova <madinauniv@gmail.com>
2025-10-08 11:26:19 +0500

    Initial commit: Create a 2nd tutorial program

---
+++ temperature.py
@@ -0,0 +1,5 @@
+celsius=float(input("Enter the temperature in Celcius: "))
+fahrenheit = (celsius * 9/5) + 32
+kelvin = celsius + 273.15
+print(f"Fahrenheit: {fahrenheit :.2f} degrees")
+print(f"Kelvin: {kelvin :.2f} degrees")
